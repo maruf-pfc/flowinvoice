f609245ce404e5505e7eb7a300999149df352d4d
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-04-21 17:19:14 +0600

    chore: reorganize project to root and consolidate stack

--- server/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-const config = {
-  port: process.env['PORT'] || 5000,
-  env: process.env['NODE_ENV'] || 'development',
-  database_url: process.env['DATABASE_URL'] || '',
-} as const;
-
-export default config;
